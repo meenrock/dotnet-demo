19efe2d799a7a3ec5d184ca208c17fee6b611396
NOVAHOrizonVelocity <77928053+meenrock@users.noreply.github.com>
2023-09-25 18:52:06 +0700

    delete cred for db

---
+++ Repositories/PostgresRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_demo.Repositories
+{
+    public class PostgresRepository : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
